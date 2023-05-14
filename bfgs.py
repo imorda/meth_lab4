dc5fe3109a6af14f3c5d8e4983251cd14e2a8744
snitron <nvar3@yandex.ru>
2023-05-14 17:45:25 +0300

    Ficed L-BFGS; Added L-BFGS description;

--- bfgs.py
+++ bfgs.py
@@ -44,57 +44,49 @@ def bfgs(f: ty.Callable, df: ty.Callable, x_0, epochs, tol=1e-6, ls_tol=1e-6):
 
 # m --- is size of "batch" of precompiled data
 def l_bfgs(f: ty.Callable, df: ty.Callable, x_0, epochs, m=5, tol=1e-9, ls_tol=1e-6):
-    E = np.eye(len(x_0))
-
-    H_i = E  # reversed (^-1) hessian
     points = [x_0]
     x_i = x_0  # point
     df_x_i = df(f, x_0)  # derivative at x_i
+    p=-df_x_i
+    t, _, _ = wolfe_conditions(f, df, x_0, p, tol = ls_tol)
 
     ys = []
     ss = []
 
-    for i in range(m):
-        p_i = -np.dot(H_i, df_x_i)
-        t, f_calls, df_calls = wolfe_conditions(f, df, x_i, p_i, tol=ls_tol)
+    ss.append(t * p)
+    x_i1 = x_i + ss[-1]
+    points.append(x_i1)
+    df_x_i1 = df(f, x_i1)
 
-        s = t * p_i
-        x_i1 = x_i + s
-        y = (df(f, x_i1) - df(f, x_i)).T
-
-        ys.append(y)
-        ss.append(s)
-
-        rho = 1 / np.dot(y.T, s)
-        m1 = E - rho * np.dot(s, y.T)
-        m2 = E - rho * np.dot(s.T, y)
-
-        H_i = np.dot(m1, np.dot(H_i, m2)) + rho * np.dot(s, s.T)
-        x_i = x_i1
-        points.append(x_i)
-        df_x_i = df(f, x_i)
-
-    i = m
+    ys.append(df_x_i1 - df_x_i)
+    df_x_i = df_x_i1
+    x_i = x_i1
 
     def alpha_calc(j, q):
-        return rho_calc(ys[-j], ss[-j]) * np.dot(ss[-j].T, q)
+        return rho_calc(ys[len(ss) - 1 - j], ss[len(ss) - 1 - j]) * np.dot(ss[len(ss) - 1 - j].T, q)
 
-    # calculating reversed hessian recursively
-    while i < epochs and np.linalg.norm(df_x_i) > tol:
+    def calcualte_p():
         q = df(f, x_i)
+        alphas = [None] * len(ss)
 
-        for j in range(1, m):
-            q = q - np.dot(alpha_calc(j, q), ys[-j])
+        for j in range(len(ss)):
+            alphas[len(ss) - 1 - j] = alpha_calc(j, q)
+            q = q - alphas[len(ss) - 1 - j] * ys[len(ss) - 1 - j]
 
         lambd = np.dot(ss[-1].T, ys[-1]) / np.dot(ys[-1].T, ys[-1])
-        H_i = lambd * E
+        H_i = lambd * np.eye(len(ss[-1]))
         p_i = np.dot(H_i, q)
 
-        for j in range(m, 0, -1):
-            betta = rho_calc(ys[-j], ss[-j]) * np.dot(ys[-j].T, p_i)
-            p_i = p_i + np.dot(ss[-j], alpha_calc(j, q) - betta)
+        for j in range(len(ss)):
+            betta = rho_calc(ys[j], ss[j]) * np.dot(ys[j].T, p_i)
+            p_i = p_i + ss[j] * (alphas[j] - betta)
 
-        p_i = -p_i
+        return -p_i
+
+    i = 0
+    # calculating reversed hessian recursively
+    while i < epochs and np.linalg.norm(df_x_i) > tol:
+        p_i = calcualte_p()
 
         t, f_calls, df_calls = wolfe_conditions(f, df, x_i, p_i, tol=1e-6)
 
@@ -105,6 +97,10 @@ def l_bfgs(f: ty.Callable, df: ty.Callable, x_0, epochs, m=5, tol=1e-9, ls_tol=1
         ys.append(y)
         ss.append(s)
 
+        if len(ss) == m + 1:
+            ys.pop(0)
+            ss.pop(0)
+
         x_i = x_i1
         points.append(x_i)
         df_x_i = df(f, x_i)
@@ -112,3 +108,9 @@ def l_bfgs(f: ty.Callable, df: ty.Callable, x_0, epochs, m=5, tol=1e-9, ls_tol=1
         i += 1
 
     return points
+
+
+def f(x):
+    return 100 * x[0] ** 2 + 200 * x[1] ** 2
+
+print(l_bfgs(f_matias, descent.numeric_gradient, x_0 = np.array((9.5, 0.8)), epochs=1000, tol=1e-6, m=10))
