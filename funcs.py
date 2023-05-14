d8e4b3094d2ff32dba41af3281c4f1cf56eba960
snitron <nvar3@yandex.ru>
2023-05-14 04:52:25 +0300

    Added BFGS and L-BFGS realisations;

---
+++ funcs.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+def f_bukin(x):
+    return 100 * np.sqrt(np.abs(x[1] - 0.01 * x[0] ** 2)) + 0.01 * np.abs(x[0] + 10)
+
+def f_matias(x):
+    return 0.28 * (x[0] ** 2 + x[1] ** 2) - 0.46 * x[0] * x[1]
